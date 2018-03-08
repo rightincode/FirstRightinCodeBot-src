0fefec7b9e15ca31eb5bbe7824fedc762100df5a
Richard L. Taylor Jr <rtaylor@sentryone.com>
2018-03-08 11:09:09 -0800

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=firstrightincodebot-Web-Deploy.pubxml -p:Password=Gtrer0BtXeC6BjwTbluK5eNFAdhJzgriJiKp9QuNGQ2kd8l17MgJh7Anwjh6
+
